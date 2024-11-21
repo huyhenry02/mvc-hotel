d77a693a36b5e89522279f75fcaa7c90300a8bd4
john <sandbox.duchuy02@gmail.com>
2024-11-21 18:16:07 +0700

    create project

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
+            $table->string('address');
+            $table->string('phone');
+            $table->enum('role_type', ['admin', 'customer'])->default('customer');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
---
+++ database/migrations/2024_11_21_092458_create_init_database.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('room_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('room_type_id')->constrained('room_types')->onDelete('cascade');
+            $table->integer('capacity');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('room_id')->constrained('rooms')->onDelete('cascade');
+            $table->date('check_in_date');
+            $table->date('check_out_date');
+            $table->decimal('total_price', 10, 2);
+            $table->enum('payment_status', ['pending', 'paid'])->default('pending');
+            $table->boolean('email_confirmed')->default(false);
+            $table->timestamps();
+        });
+
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id('transaction_id');
+            $table->foreignId('booking_id')->constrained('bookings')->onDelete('cascade');
+            $table->enum('payment_method', ['credit_card', 'paypal', 'bank_transfer']);
+            $table->dateTime('payment_date');
+            $table->decimal('amount', 10, 2);
+            $table->enum('transaction_status', ['success', 'failed', 'pending'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('bookings');
+        Schema::dropIfExists('rooms');
+        Schema::dropIfExists('room_types');
+    }
+};
